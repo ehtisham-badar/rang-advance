7808f0fdf8b618227b50017d1a932dc5f3e113b3
Ehtisham Badar <ehtisham.badar@gmail.com>
2026-04-23 01:12:09 +0500

    initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $orgcom.rang.advance.group;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
